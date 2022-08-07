6e8a66a8f43edd2b96e9bb7d5d53e152354489a5
Dennis Lien <dennis.lien.o@gmail.com>
2022-08-07 13:29:05 +0200

    Add collectionWrite interface

--- interface/collection/index.ts
+++ interface/collection/index.ts
@@ -3,13 +3,15 @@ import { Descriptor } from '../../type/index.ts';
 /**
  * CollectionsQuery messages are JSON objects that include general Message Descriptor properties
  *  and the following additional properties, which MUST be composed as follows:
- * 
+ *
  * The message object MUST contain a descriptor property,
  *  and its value MUST be a JSON object composed as follows:
- * 
- *  
+ *
+ *
  *  1.  The object MUST contain a method property,
  *          and its value MUST be the string CollectionsQuery
+ *      FROM    TYPE    DESCRIPTION
+ *
  *  2.  The object MAY contain a schema property,
  *          and if present its value MUST be a URI string that indicates the schema of the associated data
  *  3.  The object MAY contain an recordId property,
@@ -17,6 +19,8 @@ import { Descriptor } from '../../type/index.ts';
  *  4.  The object MAY contain a dataFormat property,
  *          and its value MUST be a string that indicates the format of the data in accordance with its MIME type designation.
  *          The most common format is JSON, which is indicated by setting the value of the dataFormat property to application/json
+ *      FROM    TYPE    DESCRIPTION
+ *
  *  5.  The object MAY contain a dateSort field,
  *          and if present its value MUST be one of the following strings
  *  5.1 createdAscending: return results in order from the earliest dateCreated value to the latest
@@ -33,8 +37,44 @@ export enum DateSort {
 }
 
 export interface CollectionQuery extends Descriptor {
-    schema: string; // MUST be a URI string
-    recordId: string; // MUST be a [RFC4122] UUID Version 4 string
+    schema?: string; // MUST be a URI string
+    recordId?: string; // MUST be a [RFC4122] UUID Version 4 string
     dateSort: keyof typeof DateSort;
 };
 
+/**
+ * https://identity.foundation/decentralized-web-node/spec/#write
+ * CollectionsWrite messages are JSON objects
+ *  that include general Message Descriptor properties
+ *  and the following additional properties, which MUST be composed as follows:
+ *
+ * The message object MUST contain a descriptor property,
+ *  and its value MUST be a JSON object composed as follows:
+ *
+ *  1.  The object MUST contain a method property,
+ *      and its value MUST be the string CollectionsWrite.
+ *      FROM    TYPE    DESCRIPTION
+ *
+ *  2.  The object MUST contain an recordId property,
+ *      and its value MUST be a [RFC4122] UUID Version 4 string.
+ *  3.  The object MAY contain a schema property,
+ *      and if present its value MUST be a URI string that indicates the schema of the associated data.
+ *  4.  The object MAY contain a published property,
+ *      and if present its value MUST be a boolean indicating the record’s publication state.
+ *      A value of true indicates the record has been published for public queries and consumption without requiring authorization.
+ *      A value of false or the absence of the property indicates the record
+ *      MUST NOT be served in response to public queries that lack proper authorization.
+ *  5.  The object MUST contain a dateCreated property,
+ *      and its value MUST be a Unix epoch timestamp
+ *      that MUST be set and interpreted as the time the logical entry was created by the DID owner or another permitted party.
+ *  6.  The object MAY contain a datePublished property,
+ *      and its value MUST be a Unix epoch timestamp
+ *      that MUST be set and interpreted as the time the logical entry was published by the DID owner or another permitted party.
+ */
+export interface CollectionWrite extends Descriptor {
+    recordId: string; // MUST be a [RFC4122] UUID Version 4 string
+    schema?: string; // MUST be a URI string
+    published?: boolean; // MUST be a boolean indicating the record’s publication state.
+    dateCreated: number; // MUST be a Unix epoch timestamp that MUST be set and interpreted as the time the logical entry was created by the DID owner or another permitted party.
+    datePublished: number; // MUST be a Unix epoch timestamp that MUST be set and interpreted as the time the logical entry was published by the DID owner or another permitted party.
+};
